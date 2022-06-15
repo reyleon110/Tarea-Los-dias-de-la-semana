38808912b552518decd57ee94431f9d956a0eca3
reyleon110 <106993500+reyleon110@users.noreply.github.com>
2022-06-15 14:48:57 -0600

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -1,29 +1,24 @@
 
-var dia = 3;
+<body>
 
-
-switch (dia) {
-	case 1: 
-	   console.log("lunes");
-	break;
-	case 2:
-	   console.log("martes");
-	break;
-	case 3:
-		console.log("miercoles");
-	break;
-	case 4:
-		console.log("jueves");
-	break;
-	case 5:
-		console.log("viernes");
-	break;
-	case 6:
-		console.log("sabado");
-	break;
-	case 7:
-		console.log("domingo");
-	break;
-default:
-	console.log("Gracias por elegir este dia");
-}
+<h1> LOS DIAS DE LA SEMANA</h1>
+<body>
+        </htnl>
+<h2> Lunes </h2>
+<h3>  En la mañana </h3>
+<h4> Los lunes inicio mi semana tomando cafe, normalmente voy al trabajo en bicicleta </h4>
+<h3> En la tarde </h3>
+<h4> En la tarde </h3>
+<h2> Martes </h2>
+<h3> En la mañana </h3>
+<h4> Desayuno frutas y verduras </h4>
+<h3> Miercoles </h3>
+<h4> hice lo mismo que ayer</h4>
+<h3> Jueves </h3>
+<h4> A que no adivinan </h4>
+<h3> Viernes </h3>
+<h4> no hice nada </h4>
+<h3> Sabado </h3>
+<h4> tampoco hoy </h4>
+<h3> Domingo </h3>
+<h4> feliz fin de semana </h4>
